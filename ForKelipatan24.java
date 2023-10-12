ffbfbaacc10dd16a1e7a226a6154502d39d0f9a2
Ruphasa <rfrizqifauzan@gmail.com>
2023-10-12 19:06:48 +0700

    ngehapus s tadi

--- ForKelipatan24.java
+++ ForKelipatan24.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class ForKelipatan24 {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah=0, counter=0;
@@ -13,6 +12,6 @@ public class ForKelipatan24 {
             }
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 ada %d\n", kelipatan, jumlah);S
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 ada %d\n", kelipatan, jumlah);
     }
 }
